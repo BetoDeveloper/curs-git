7ef188f6de4d465a42e1739c97a86713e4abae80
Beto <gilto37@hotmail.com>
2019-09-29 10:27:26 -0300

    Acrescentada a menssagem desejando um bom dia

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -11,8 +11,9 @@ namespace Teste
         static void Main(string[] args)
         {
             Console.WriteLine("Ola Mundo");
-            Console.WriteLine("Bom Dia");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia");
+            Console.ReadLine();
         }
         
     }

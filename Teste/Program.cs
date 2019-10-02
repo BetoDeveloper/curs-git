1adf53d5184b3ccd0791d877ea9a412671743bde
Beto <gilto37@hotmail.com>
2019-10-02 18:46:28 -0300

    Acrescentada a menssagem de bom dia

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -10,11 +10,14 @@ namespace Teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("------------------------");
             Console.WriteLine("Ola Mundo");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia");
             Console.WriteLine("Tchau");
+            Console.WriteLine("------------------------");
             Console.ReadLine();
+           
         }
         
     }
